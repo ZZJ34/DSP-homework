4a9575683c3c758c86d941aaad36566e3bc39290
zzj34 <zhaozjseu@163.com>
2020-05-30 17:20:32 +0800

    实验3第二题

--- exp_3/xb.m
+++ exp_3/xb.m
@@ -1,7 +1,7 @@
 function Xb = xb(n,a,f)
 for i = 1:length(n)
     if n(i) >= 0 && n(i) <= 15
-    Xb(i) = exp(-a*n(i)) * sin (2 * pi * f * n);
+    Xb(i) = exp(-a*n(i)) * sin (2 * pi * f * n(i));
     else
     Xb(i) = 0;
     end
